51a6fda4de9aa0d9ac9953075c166ee934ad0f6d
EleanorCallottWhitney <eleanor.whitney@gmail.com>
2014-01-04 18:08:13 -0500

    adding movies app

---
+++ __eleanor_whitney/movie_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MovieApp::Application.config.secret_key_base = 'ed12406643583d7fd37ba47b79e8cc3d933caf3cd6ce887e0c1ce25db1c75e6d537f06b374a9f781600ebf57658bf9607f2804e77686a78f6bf154e2d3acc183'
